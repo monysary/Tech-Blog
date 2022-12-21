cbce510a99ccfcce95507403651ea3127506e97e
monysary <sary.mony@gmail.com>
2022-12-21 15:37:09 -0800

    Fixed placement of hooks in user.js

--- models/User.js
+++ models/User.js
@@ -26,21 +26,19 @@ User.init(
             }
         }
     },
-    // {
-    //     hooks: {
-    //         beforeCreate: async (newUser) => {
-    //             newUser.password = await bcrypt.hash(newUser.password, 10);
-    //             return newUser
-    //         },
-    //         beforeUpdate: async (updatedUser) => {
-    //             if (updatedUser.password) {
-    //                 updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
-    //             }
-    //             return updatedUser;
-    //         }
-    //     }
-    // },
     {
+        hooks: {
+            beforeCreate: async (newUser) => {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+                return newUser
+            },
+            beforeUpdate: async (updatedUser) => {
+                if (updatedUser.password) {
+                    updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+                }
+                return updatedUser;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
