1fb5b5425add665ca40dd1d3ff936f2a312e6cff
monysary <sary.mony@gmail.com>
2022-12-21 19:00:27 -0800

    Set up handlebars routes

--- server.js
+++ server.js
@@ -2,20 +2,20 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
 const path = require('path');
 
-const hbs = exphbs.create({})
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Setting up handlebars engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers'))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers'))
 
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log(`Application listening at http://localhost:${PORT}`));
