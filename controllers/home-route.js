650107c7cee4e1ec5a675b46f28b37140faccc5e
monysary <sary.mony@gmail.com>
2023-01-05 15:47:55 -0800

    Added dashboard page

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -60,4 +60,25 @@ router.get('/posts/:id', async (req, res) => {
     }
 })
 
+// Render the user's dashboard
+router.get('/dashboard', async (req, res) => {
+    try {
+        const userBlogData = await BlogEntry.findAll({
+            where: {
+                user_id: req.session.userID
+            }
+        });
+
+        const userBlog = userBlogData.map((blog) => {
+            blog.get({ plain: true });
+        })
+
+        res.render('dashboard', { userBlog, loggedIn: req.session.loggedIn })
+
+    } catch (err) {
+        console.log(err);
+        res.status(404).json(err);
+    }
+})
+
 module.exports = router;
