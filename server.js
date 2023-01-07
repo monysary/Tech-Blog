e16c6ff4c50a0fdbfe416feab4c078cd322fcfb9
monysary <sary.mony@gmail.com>
2023-01-06 18:52:09 -0800

    organized server.js

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
 // Setting up dependencies
 const express = require('express');
-const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
 const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers })
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Setting up handlebars engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Setting up sessions
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
     cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
     },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db: sequelize
     })
-  };
+};
 
 app.use(session(sess));
+
+const hbs = exphbs.create({ helpers })
+
+// Setting up handlebars engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Setting up express middleware
 app.use(express.json());
